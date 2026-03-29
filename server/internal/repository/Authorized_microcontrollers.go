8fce22561eb1a6412e3f84fd7cc75876918ee0e4
George Klados <kladgeo@gmail.com>
2026-03-30 01:14:07 +0300

    [BE] fix GetAuthorizedMicrocontrollerByDeviceId method

--- server/internal/repository/Authorized_microcontrollers.go
+++ server/internal/repository/Authorized_microcontrollers.go
@@ -17,21 +17,19 @@ type AuthorizedMicrocontrollers struct {
 func (am *AuthorizedMicrocontrollers) GetAuthorizedMicrocontrollerByDeviceId(deviceId string) (model.AuthorizedMicrocontroller, error) {
 	var microcontroller model.AuthorizedMicrocontroller
 
-	docs, err := am.DB.Collection(authorizedMicrocontrollersCollectionName).
-		Where("device_id", "==", deviceId).
-		Limit(1).
-		Documents(context.Background()).
-		GetAll()
+	doc, err := am.DB.Collection(authorizedMicrocontrollersCollectionName).
+		Doc(deviceId).
+		Get(context.Background())
 
 	if err != nil {
-		return microcontroller, fmt.Errorf("error querying documents: %w", err)
+		return microcontroller, fmt.Errorf("error retrieving document: %w", err)
 	}
 
-	if len(docs) == 0 {
+	if !doc.Exists() {
 		return microcontroller, fmt.Errorf("device not found")
 	}
 
-	if err := docs[0].DataTo(&microcontroller); err != nil {
+	if err := doc.DataTo(&microcontroller); err != nil {
 		return microcontroller, fmt.Errorf("error unmarshalling document: %w", err)
 	}
 
